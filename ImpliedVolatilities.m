22e332c334356d00c7787d8d578439e30c4bb84f
KatieD11 <kathryndvs11@gmail.com>
2023-09-30 16:36:04 +0200

    Compute implied vols for all maturities in dataset

--- ImpliedVolatilities.m
+++ ImpliedVolatilities.m
@@ -1,5 +1,6 @@
 % Implied volatilities
-% Test if plots in report outline can be replicated
+% Compute implied volatilies using SSVI formulation 
+% with parameters from calibration
 clear; clc;
 addpath('./Data_prep');
 
@@ -9,25 +10,35 @@ calibration_params=readtable("Calibration_results/spx_20220401_calibration_param
 %calibration_params=readtable("Calibration_results/spx_20220401_calibration_params_without_weights.csv");
 bid_ask_spread=readtable("Calibration_results/spx_20220401_calibration_bid_ask_spread.csv");
 
-S0 = 4545.86;
+S0 = 4545.86; % spx_20220401
+%% Compute implied vols for all maturities in dataset
+SSVI_vols_cellArray = {}; % Store arrays of different lengths
+ks_cellArray = {};
 
+T_maturities = discountData_df.T; 
+for i = 1:length(T_maturities)
+    T_i = T_maturities(i);
+    ks = spx_df.logStrike(spx_df.TimeToExpiration == T_i);
+    thetaT = discountData_df.TotImplVar(discountData_df.T == T_i);
+    SSVI_vols = SSVIimpliedVolatility(thetaT, T_i, ks, ...
+            calibration_params.rho, calibration_params.eps);
+    % Store arrays
+    ks_cellArray{end+1} = ks;
+    SSVI_vols_cellArray{end+1} = SSVI_vols;
+end
+
+%% Plot results for a particular maturity T
 % Choose a time to maturity in days
 Tn_days = 90;
+%Tn_days = 105; % [17, 19, 21, 24, 26, 28, 31, 35, 42, 49 ..., ]
 
 % Get the time to expiration in days (approx)
 spx_df.T_days = round(spx_df.TimeToExpiration*365);
 discountData_df.T_days = round(discountData_df.T*365);
 bid_ask_spread.T_days = round(bid_ask_spread.TimeToExpiration*365);
 
-% Compute implied vols using SVI framework for Tn_days
-ks = spx_df.logStrike(spx_df.T_days == Tn_days);
-SSVI_vols = zeros(length(ks),1);
-thetaT = discountData_df.TotImplVar(discountData_df.T_days == Tn_days);
-T_i = discountData_df.T(discountData_df.T_days == Tn_days);
-for j=1:length(ks)
-    SSVI_vols(j) = SSVIimpliedVolatility(thetaT, T_i, ks(j), ...
-        calibration_params.rho, calibration_params.eps);
-end
+% Strike values for the maturity
+ks = ks_cellArray{discountData_df.T_days == Tn_days};
 
 filter = spx_df.T_days == Tn_days;
 figure(1)
@@ -39,7 +50,8 @@ plot(ks, spx_df.putBid_BSvol(filter), ".",'Color',"#0072BD");
 hold on
 plot(ks, spx_df.putAsk_BSvol(filter), ".",'Color',"#A2142F");
 hold on
-plot(ks, SSVI_vols, "-g", "LineWidth",1);
+%plot(ks, SSVI_vols, "-g", "LineWidth",1);
+plot(ks, SSVI_vols_cellArray{discountData_df.T_days == Tn_days}, "-g", "LineWidth",1);
 hold on
 plot(bid_ask_spread.logStrike(bid_ask_spread.T_days == Tn_days), ...
     bid_ask_spread.sigma_target(bid_ask_spread.T_days== Tn_days), ...
@@ -49,5 +61,9 @@ xlabel("Log strike")
 ylabel("BS implied vol")
 legend(["Call bid", "Call ask", "Put bid", "Put ask", "SSVI", "Target"])
 title("SPX: maturity " +Tn_days+ " days (2022-4-1)")
-%% 
-% Try create a surface plot
+%% Implied variance plot
+
+% Implied variance plot
+
+plot(ks, (SSVI_vols.^2)*(Tn_days/365), "-g", "LineWidth",1);
+
--- SSVIimpliedVolatility.m
+++ SSVIimpliedVolatility.m
@@ -10,7 +10,7 @@ function impliedVol = SSVIimpliedVolatility(thetaT, T, k, rho, eps)
     phi = @(eta, theta) eta/(theta^gamma1*(1+beta1*theta)^gamma2* ...
         (1+beta2*theta)^(1-gamma1-gamma2));
     w = thetaT/2*(1+rho*phi(eta,thetaT)*k + ...
-        sqrt((phi(eta,thetaT)*k + rho)^2 + (1-rho^2)));
+        sqrt((phi(eta,thetaT)*k + rho).^2 + (1-rho^2)));
 
     % Implied volatility
     impliedVol = sqrt(w/T);
