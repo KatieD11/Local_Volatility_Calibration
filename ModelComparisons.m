e42bcf1f0eee6dedd8acaac8050fdde438d78d56
KatieD11 <kathryndvs11@gmail.com>
2023-10-04 20:20:24 +0200

    Add script to compare calibration results

---
+++ ModelComparisons.m
@@ -0,0 +1,87 @@
+% Model comparison 
+% Uses results from the ImpliedVolatility script
+clear; clc;
+
+% Select dataset
+dataset = "spx_20220401";
+
+% Load option datasets
+spx_df=readtable("Data_prep/Data/"+dataset+"_filtered_optionDataWithImplVol.csv");
+discountData_df=readtable("Data_prep/Data/"+dataset+"_discountData.csv");
+bid_ask_spread=readtable("Data_prep/Data/"+dataset+"_option_bid_ask_spread.csv");
+
+% Load SSVI implied volatility results for each model
+SSVIvols_withWeights_data = load('Calibration/Calibration_results/'+dataset+'_SSVIimpliedVols_with_weights.mat');
+SSVIvols_withWeights_cellArray = SSVIvols_withWeights_data.SSVI_vols_cellArray;
+SSVIvols_freeParams_data = load('Calibration/Calibration_results/'+dataset+'_SSVIimpliedVols_with_free_params.mat');
+SSVIvols_freeParams_cellArray = SSVIvols_freeParams_data.SSVI_vols_cellArray;
+SSVIvols_heston_data = load('Calibration/Calibration_results/'+dataset+'_SSVIimpliedVols_heston.mat');
+SSVIvols_heston_cellArray = SSVIvols_heston_data.SSVI_vols_cellArray;
+SSVIvols_powerLaw_data = load('Calibration/Calibration_results/'+dataset+'_SSVIimpliedVols_powerLaw.mat');
+SSVIvols_powerLaw_cellArray = SSVIvols_powerLaw_data.SSVI_vols_cellArray;
+%% Plot implied volatility results for a particular maturity T
+% Choose a time to maturity in days
+Tn_days = 90;
+%Tn_days = 259; % [17, 19, 21, 24, 26, 28, 31, 35, 42, 49 ..., ]
+
+% Get the time to expiration in days (approx)
+spx_df.T_days = round(spx_df.TimeToExpiration*365);
+discountData_df.T_days = round(discountData_df.T*365);
+bid_ask_spread.T_days = round(bid_ask_spread.TimeToExpiration*365);
+
+% Strike values for the maturity
+ks = bid_ask_spread.logStrike(bid_ask_spread.T_days == Tn_days);
+
+filter = spx_df.T_days == Tn_days;
+figure(1)
+plot(ks, spx_df.callBid_BSvol(filter), ".", 'color', [.5 .5 .5]);
+hold on
+plot(ks, spx_df.callAsk_BSvol(filter), ".", 'color', [.5 .5 .5]);
+hold on
+plot(ks, spx_df.putBid_BSvol(filter), ".",'color', [.5 .5 .5]);
+hold on
+plot(ks, spx_df.putAsk_BSvol(filter), ".",'color', [.5 .5 .5]);
+hold on
+plot(ks, SSVIvols_withWeights_cellArray{discountData_df.T_days == Tn_days}, "-g", "LineWidth",1);
+hold on
+plot(ks, SSVIvols_freeParams_cellArray{discountData_df.T_days == Tn_days}, "-r", "LineWidth",1);
+hold on
+plot(ks, SSVIvols_heston_cellArray{discountData_df.T_days == Tn_days}, "-b", "LineWidth",1);
+hold on
+plot(ks, SSVIvols_powerLaw_cellArray{discountData_df.T_days == Tn_days}, "-m", "LineWidth",1);
+hold on
+plot(bid_ask_spread.logStrike(bid_ask_spread.T_days == Tn_days), ...
+    bid_ask_spread.sigma_target(bid_ask_spread.T_days== Tn_days), ...
+    "-c", "LineWidth",1);
+hold off
+xlabel("Log strike")
+ylabel("BS implied vol")
+legend(["Call bid", "Call ask", "Put bid", "Put ask", ...
+    "SSVI (with weights)", "SSVI (free parameters)", ...
+    "SSVI (heston-like)", "SSVI (power-law)","Target"])
+title("Implied volatilities for maturity " +Tn_days+ " days")
+%% Plot MAPEs (Mean absolute percentage errors) vs maturity
+% Load MAPEs for each model
+mapes_withWeights=readtable("Calibration/Calibration_results/"+dataset+"_mapes_with_weights.csv");
+mapes_freeParams=readtable("Calibration/Calibration_results/"+dataset+"_mapes_with_free_params.csv");
+mapes_heston=readtable("Calibration/Calibration_results/"+dataset+"_mapes_heston.csv");
+mapes_powerLaw=readtable("Calibration/Calibration_results/"+dataset+"_mapes_powerLaw.csv");
+
+T_maturities = mapes_withWeights.T;
+
+figure(2)
+plot(T_maturities, mapes_withWeights.mape, "xg", "LineWidth",2)
+hold on
+plot(T_maturities, mapes_freeParams.mape, "xr", "LineWidth",2)
+hold on
+plot(T_maturities, mapes_heston.mape, "xb", "LineWidth",2)
+hold on
+plot(T_maturities, mapes_powerLaw.mape, "xm", "LineWidth",2)
+hold off
+title("Mean absolute percentage errors of SSVI vols vs target implied vols")
+xlabel("Maturity (T)")
+ylabel("MAPE (%)")
+legend(["SSVI (with weights)", "SSVI (free parameters)", ...
+    "SSVI (heston-like)", "SSVI (power-law)"])
+
+
