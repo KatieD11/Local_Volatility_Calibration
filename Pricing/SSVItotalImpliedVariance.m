926520b549251c50e83e8db476bf5550e9ac2d42
KatieD11 <kathryndvs11@gmail.com>
2023-11-29 15:37:58 +0200

    Additional check for butterfly arbitrage

--- Pricing/SSVItotalImpliedVariance.m
+++ Pricing/SSVItotalImpliedVariance.m
@@ -16,4 +16,14 @@ function w = SSVItotalImpliedVariance(discountData_df, T, k, rho, eps, ...
     w = thetaT/2.*(1+rho.*phi(eta,thetaT).*k + ...
         sqrt((phi(eta,thetaT).*k + rho).^2 + (1-rho.^2)));
 
+    % Conditions for absence of butterfly arbitrage:
+    % i)  θϕ(θ) (1 + |ρ|)  <  4,  for  all  θ  >  0
+    % ii) θϕ(θ)^2  (1 + |ρ|)  ≤  4,  for  all  θ  >  0
+    if (thetaT.*phi(eta,thetaT)*(1+abs(rho)) >= 4)
+        disp("Butterfly arbitrage: condition 1 violated")
+    end
+    if (thetaT.*phi(eta,thetaT).^2*(1+abs(rho)) > 4)
+        disp("Butterfly arbitrage: condition 2 violated")
+    end
+
 end
