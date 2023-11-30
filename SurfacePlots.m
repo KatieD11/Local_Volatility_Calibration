fd11875d87c462a36634e62c2d3c2f9c9fcac79f
KatieD11 <kathryndvs11@gmail.com>
2023-11-30 11:37:26 +0200

    Add plots to show butterfly arbitrage-free conditions

--- SurfacePlots.m
+++ SurfacePlots.m
@@ -26,6 +26,7 @@ impliedVolsPWR = zeros(length(T), length(k));
 
 totImplVarHST = zeros(length(T), length(k)); % Plot for report (See ImpliedVolatilies file for testing the condition)
 totImplVarPWR = zeros(length(T), length(k));
+totImplVarSPX = zeros(length(T), length(k));
 
 % Interpolate to get thetaT values at different maturities
 thetaT_fnc = @(T) interp1(T_maturities,discountData_df.TotImplVar,T, 'linear');
@@ -47,6 +48,7 @@ for i = 1:length(T)
                 calibration_params_SPX.rho, calibration_params_SPX.eps, ...
                 calibration_params_SPX.gamma1, calibration_params_SPX.gamma2, ...
                 calibration_params_SPX.beta1, calibration_params_SPX.beta2);
+        totImplVarSPX(i,:) =impliedVolsSPX(i,:).^2*T_i;
     
 end
 
@@ -92,22 +94,40 @@ hold off; % Release the hold on the current axes
 %% Implied variance plot (for HST surface)
 %totImplVarHST
 figure(5)
+numLines = length(T);
+colorMap = winter(numLines);
 for i = 1:length(T)
     T_i = T(i);
-    plot(k, totImplVarHST(i,:), "-", "LineWidth",1);
+    color = colorMap(i, :);
+    plot(k, totImplVarHST(i,:), "-", "LineWidth",1, 'Color', color);
     hold on
 end
 hold off
-legend(string(T))
+%legend(string(T))
 title("Total implied variance plot for the Heston-like surface")
 xlabel("Log strike")
 ylabel("Total implied variance")
-
-%% Implied variance plot (for PWR surface)
+exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'totImplVarHST.pdf','ContentType','vector')
+%% Implied variance plot (for SPX-fit surface)
 figure(6)
+for i = 1:numLines
+    T_i = T(i);
+    color = colorMap(i, :); 
+    plot(k, totImplVarSPX(i, :), "-", "LineWidth", 1, 'Color', color);
+    hold on
+end
+hold off
+%legend(string(T))
+title("Total implied variance plot for the SPX-fit surface")
+xlabel("Log strike")
+ylabel("Total implied variance")
+exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'totImplVarSPX.pdf','ContentType','vector')
+%% Implied variance plot (for PWR surface)
+figure(7)
 for i = 1:length(T)
     T_i = T(i);
-    plot(k, totImplVarPWR(i,:), "-", "LineWidth",1);
+    color = colorMap(i, :);
+    plot(k, totImplVarPWR(i,:), "-", "LineWidth",1, 'Color', color);
     hold on
 end
 hold off
@@ -115,11 +135,14 @@ hold off
 title("Total implied variance plot for the power-law surface")
 xlabel("Log strike")
 ylabel("Total implied variance")
+exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'totImplVarPWR.pdf','ContentType','vector')
 %% Check subset of the smaller maturities
-figure(7)
+figure(8)
+colorMap = winter(18);
 for i = 1:18
     T_i = T(i);
-    plot(k, totImplVarPWR(i,:), "-", "LineWidth",1);
+    color = colorMap(i, :);
+    plot(k, totImplVarPWR(i,:), "-", "LineWidth",1, 'Color', color);
     hold on
 end
 hold off
@@ -127,3 +150,80 @@ hold off
 title("Total implied variance plot for the power-law surface, maturities 17 to 80 days")
 xlabel("Log strike")
 ylabel("Total implied variance")
+exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'totImplVarPWR2.pdf','ContentType','vector')
+%% Check conditions for absence of butterfly arbitrage:
+    % i)  θϕ(θ) (1 + |ρ|)  <  4,  for  all  θ  >  0
+    % ii) θϕ(θ)^2  (1 + |ρ|)  ≤  4,  for  all  θ  >  0
+theta = discountData_df.TotImplVar;
+%theta = (min(discountData_df.TotImplVar):0.005:0.1)';
+
+% Power-law surface
+phi_PWR = calibration_params_PWR.eta./(theta.^calibration_params_PWR.gamma.*(1+theta).^(1-calibration_params_PWR.gamma));
+C1 = theta.*phi_PWR*(1+abs(calibration_params_PWR.rho));
+C2 = theta.*phi_PWR.^2*(1+abs(calibration_params_PWR.rho));
+
+figure(9)
+yline(4)
+hold on
+plot(theta, C1)
+plot(theta, C2)
+hold off
+title("Power-law surface: Absence of butterfly arbitrage conditions")
+xlabel("θ")
+ylabel("Value of C1/C2")
+legend(["","C1: θ ϕ(θ) (1 + |ρ|)", "C2: θ ϕ(θ)^2  (1 + |ρ|)"])
+%exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'butterflyPWR.png')
+
+% Heston-like surface
+phi_HST = 1./(calibration_params_HST.lambda*theta).* ... 
+        (1-(1-exp(-calibration_params_HST.lambda*theta))./...
+        (calibration_params_HST.lambda*theta));
+C1 = theta.*phi_HST*(1+abs(calibration_params_HST.rho));
+C2 = theta.*phi_HST.^2*(1+abs(calibration_params_HST.rho));
+
+figure(10)
+yline(4)
+hold on
+plot(theta, C1)
+plot(theta, C2)
+hold off
+title("Heston-like surface: Absence of butterfly arbitrage conditions")
+xlabel("θ")
+ylabel("Value of C1/C2")
+legend(["","C1: θ ϕ(θ) (1 + |ρ|)", "C2: θ ϕ(θ)^2  (1 + |ρ|)"])
+%exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'butterflyHST.png')
+
+figure(11)
+plot(theta, C1)
+hold on
+plot(theta, C2)
+hold off
+title("Heston-like surface: Absence of butterfly arbitrage conditions")
+xlabel("θ")
+ylabel("Value of C1/C2")
+legend(["C1: θ ϕ(θ) (1 + |ρ|)", "C2: θ ϕ(θ)^2  (1 + |ρ|)"])
+%exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'butterflyHST2.png')
+
+% SPX-fit surface
+eta = 2.016048*exp(calibration_params_SPX.eps);
+phi_SPX = eta/(theta.^calibration_params_SPX.gamma1.*...
+    (1+calibration_params_SPX.beta1*theta).^calibration_params_SPX.gamma2.* ...
+        (1+calibration_params_SPX.beta2*theta).^...
+        (1-calibration_params_SPX.gamma1-calibration_params_SPX.gamma2));
+C1 = theta.*phi_SPX*(1+abs(calibration_params_SPX.rho));
+C2 = theta.*phi_SPX.^2*(1+abs(calibration_params_SPX.rho));
+
+figure(12)
+yline(4)
+hold on
+plot(theta, C1)
+plot(theta, C2)
+hold off
+title("SPX-fit surface: Absence of butterfly arbitrage conditions")
+xlabel("θ")
+ylabel("Value of C1/C2")
+legend(["","C1: θ ϕ(θ) (1 + |ρ|)", "C2: θ ϕ(θ)^2  (1 + |ρ|)"])
+%exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'butterflySPX.pdf','ContentType','vector')
+exportgraphics(gcf,'Calibration/Calibration_results/'+dataset+'butterflySPX.png')
+
+
